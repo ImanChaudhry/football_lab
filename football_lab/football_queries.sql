b06c06f06156661ab07ff40826bc19d90e987092
Iman Chaudhry <i-man-735@hotmail.com>
2022-05-13 15:37:15 +0100

    first commit

---
+++ football_lab/football_queries.sql
@@ -0,0 +1,9 @@
+-- Find all the matches from 2017.
+SELECT
+
+-- Find all the matches featuring Barcelona.
+
+-- What are the names of the Scottish divisions included?
+
+-- Find the division code for the Bundesliga. Use that code to find out how many matches Freiburg 
+-- have played in the Bundesliga since the data started being collected.
